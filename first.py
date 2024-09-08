6c4a8b2a0b3ecc8b2400eae5f71f8786f06f5f26
Iamsunil06 <sunilyadav67804@gmail.com>
2024-09-08 17:22:03 +0530

    first

---
+++ first.py
@@ -0,0 +1,17 @@
+
+# print("hello world");
+# print("god always with me");
+# print("radhe radhe");
+# print("god always with me","radhe radhe");
+
+
+# name = "sunil yadav"
+# age = 20
+# price = 500
+# print(price,name,age)
+
+a = 50
+b = 20
+sum = a-b
+print(sum)
+
